8abc34c065a4f64672b4ce403bc0218c931d0426
MathiasKoelleda <IT-Bereich-mathias-linke@online.de>
2021-06-30 13:47:53 +0200

    rtb-fertig

--- Vagrantfile
+++ Vagrantfile
@@ -9,6 +9,18 @@ Vagrant.configure("2") do |config|
       vb.name = "docker1"
       vb.cpus = "2"
       vb.memory = "4096"
+      sudo apt-get update
+
+      sudo apt-get install \
+         apt-transport-https \
+         ca-certificates \
+         curl \
+         gnupg \
+         lsb-release
+         sudo apt-get update
+
+         sudo apt-get install docker-ce docker-ce-cli containerd.io
+         sudo apt-get install docker-ce=<VERSION_STRING> docker-ce-cli=<VERSION_STRING> containerd.io        
     end
     docker1.vm.provision "shell" , path: "scripts/docker-provision.sh"
   end
--- scripts/docker-provision.sh
+++ scripts/docker-provision.sh
@@ -1,12 +1,43 @@
 #!/bin/sh
 
-# APT im nicht-interaktiven Modus
+# APT im nicht interaktiven Modus
 export DEBIAN_FRONTEND=noninteractive
 
-# System aktualiosieren
-apt-get update && apt-get -y dist-upgrade && apt-get --purge -y autoremove
+# Docker installieren
+apt-get -y install apt-transport-https ca-certificates curl \
+    gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | 
+    sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get -y install docker-ce docker-ce-cli containerd.io
 
-#Docker installieren
-apt-get -y install docker.io
 systemctl enable --now docker
 adduser vagrant docker
+
+# Defaultroute setzen
+ip route replace default via 192.168.100.2
+ip route delete default via 10.0.2.2
+
+# Konfiguration der Namensauflösung
+systemctl disable --now systemd-resolved
+rm /etc/resolv.conf
+echo "nameserver 192.168.100.2" > /etc/resolv.conf
+echo "search kurs.iad" >> /etc/resolv.conf
+
+    © 2021 GitHub, Inc.
+    Terms
+    Privacy
+    Security
+    Status
+    Docs
+
+    Contact GitHub
+    Pricing
+    API
+    Training
+    Blog
+    About
+
